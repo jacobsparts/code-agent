093ffd234d2116f7a0eed990d1c8ce3eab9581ed
Jacob Stoner <dev@jacobstoner.com>
2026-07-30 16:55:37 -0400

    context window specification

--- code_agent/llm_registry.py
+++ code_agent/llm_registry.py
@@ -168,18 +168,21 @@ register_model(
     "composer-2.5",
     model="composer-2.5",
     tool_mode="repl_execute",
+    context_window=200_000,
 )
 register_model(
     "cursor",
     "grok-4.5",
     model="cursor-grok-4.5-high",
     tool_mode="repl_execute",
+    context_window=256_000,
 )
 register_model(
     "cursor",
     "kimi-k3",
     model="kimi-k3-high",
     tool_mode="repl_execute",
+    context_window=200_000,
 )
 
 
